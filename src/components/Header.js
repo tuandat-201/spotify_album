24f548c9ee665105231e33cda44f3165da35bb85
Le Dat <tuandat071101@gmail.com>
2023-05-08 21:11:24 +0700

    change url

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,16 +8,13 @@ function Header(props) {
       />
       {props.token === "" ? (
         <a
-          href="https://accounts.spotify.com/authorize?response_type=token&client_id=f702a0640fac4e2ebb17f91691b971d1&scope=user-library-modify%20user-library-read&redirect_uri=https://spotify-add-album.netlify.app"
+          href="https://accounts.spotify.com/authorize?response_type=token&client_id=f702a0640fac4e2ebb17f91691b971d1&scope=user-library-modify%20user-library-read&redirect_uri=https://spotifyadd.netlify.app"
           className="account-link"
         >
           Login
         </a>
       ) : (
-        <a
-          href="https://spotify-add-album.netlify.app/"
-          className="account-link"
-        >
+        <a href="https://spotifyadd.netlify.app" className="account-link">
           Log out
         </a>
       )}
